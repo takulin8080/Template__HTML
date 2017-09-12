d923b121c8771f2ef551eb58e6b5c2c71d8859fb
taku <takulin8080@gmail.com>
2017-09-12 13:00:08 +0900

    [update] modal.js  clean up

--- project/dev/src/common/js/component/modal.js
+++ project/dev/src/common/js/component/modal.js
@@ -8,31 +8,32 @@ var overlay = require('../component/overlay');
 // -----------------------------------------------
 // variable
 // -----------------------------------------------
-var trg = $('[data-modal-trg]');
-var tar = $('[data-modal-tar]');
+var trg = 'data-modal-trg';
+var tar = 'data-modal-tar';
+var active = 'data-is-active';
 // -----------------------------------------------
 // function
 // -----------------------------------------------
-trg.click(function() {
-	var id = $(this).attr('data-modal-trg');
-	if($(this).attr('data-is-active') == 'true') {
-		$('[data-modal-trg~="' + id + '"]').attr('data-is-active', 'false');
-		$('[data-modal-tar~="' + id + '"]').attr('data-is-active', 'false');
+$('[' + trg + ']').click(function() {
+	var id = $(this).attr(trg);
+	if($(this).attr(active) == 'true') {
+		$('[' + trg + '~=' + id + ']').attr(active, 'false');
+		$('[' + tar + '~=' + id + ']').attr(active, 'false');
 		overlay(false);
 	} else {
-		$(this).attr('data-is-active', 'true');
-		$('[data-modal-trg~="' + id + '"]').attr('data-is-active', 'true');
-		$('[data-modal-tar~="' + id + '"]').attr('data-is-active', 'true');
+		$(this).attr(active, 'true');
+		$('[' + trg + '~=' + id + ']').attr(active, 'true');
+		$('[' + tar + '~=' + id + ']').attr(active, 'true');
 		overlay(true);
 	}
 });
 $(window).resize(function() {
-	trg.attr('data-is-active', 'false');
-	tar.attr('data-is-active', 'false');
+	$('[' + trg + ']').attr(active, 'false');
+	$('[' + tar + ']').attr(active, 'false');
 	overlay(false);
 });
 $('[data-overlay-layer]').click(function() {
-	trg.attr('data-is-active', 'false');
-	tar.attr('data-is-active', 'false');
+	$('[' + trg + ']').attr(active, 'false');
+	$('[' + tar + ']').attr(active, 'false');
 	overlay(false);
 });
