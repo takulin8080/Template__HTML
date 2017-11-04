405d982d654987d521d2a8c2f724d28f1be715dc
taku <takulin8080@gmail.com>
2017-11-04 16:29:50 +0900

    [fix] global-nav mdAndUnder  サイズで上手く動作していなかった
    mdUndAnderサイズだと二度クリックしないと動作しなかったため修正。

--- project/dev/src/common/js/module/global-nav.js
+++ project/dev/src/common/js/module/global-nav.js
@@ -12,10 +12,17 @@ var trg = $('.global-nav__trg');
 var tar = $('.global-nav');
 var catTrg = $('.global-nav-cat__title');
 var catTar = $('.global-nav-cat__items');
+<<<<<<< Updated upstream
+=======
+var winWidth = $(window).width();
+var winWidthResized;
+>>>>>>> Stashed changes
 // -----------------------------------------------
 // function
 // -----------------------------------------------
 trg.click(function() {
+	catTrg.attr('data-is-active', 'false');
+	catTar.attr('data-is-active', 'false');
 	if($(this).attr('data-is-active') == 'true') {
 		trg.attr('data-is-active', 'false');
 		tar.attr('data-is-active', 'false');
@@ -27,14 +34,28 @@ trg.click(function() {
 	}
 });
 $(window).resize(function() {
+<<<<<<< Updated upstream
 	trg.attr('data-is-active', 'false');
 	tar.attr('data-is-active', 'false');
 	catTrg.attr('data-is-active', 'false');
 	catTar.attr('data-is-active', 'false');
 	overlay(false);
 	trg
+=======
+	winWidthResized = $(window).width();
+	if(winWidth != winWidthResized) {
+		trg.attr('data-is-active', 'false');
+		tar.attr('data-is-active', 'false');
+		catTrg.attr('data-is-active', 'false');
+		catTar.attr('data-is-active', 'false');
+		overlay(false);
+		winWidth = $(window).width();
+	}
+>>>>>>> Stashed changes
 });
 $('[data-overlay-layer]').click(function() {
+	catTrg.attr('data-is-active', 'false');
+	catTar.attr('data-is-active', 'false');
 	trg.attr('data-is-active', 'false');
 	tar.attr('data-is-active', 'false');
 	overlay(false);
