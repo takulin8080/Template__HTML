145a084219ed9b1ec2bb435948a9379daa2dd462
Taku Suzuki <takulin8080@gmail.com>
2017-11-09 10:57:49 +0900

    [update] gulp

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -147,8 +147,6 @@ gulp.task('page', ['fileSetup'], function() {
 	})).pipe($.prettify({
 		indent_char: '\t',
 		indent_size: 1
-	})).pipe($.changed(dst, {
-		hasChanged: $.changed.compareSha1Digest
 	})).pipe(gulp.dest(dst));
 });
 gulp.task('fileSetup', function() {
