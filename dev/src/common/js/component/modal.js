5686662382449dcb205fd20cb565c2cc689c9c8f
taku <takulin8080@gmail.com>
2018-01-23 13:03:17 +0900

    [update] modal 初期表示、javascript処理対応

--- dev/src/common/js/component/modal.js
+++ dev/src/common/js/component/modal.js
@@ -12,8 +12,24 @@ var trg = 'data-modal-trg';
 var tar = 'data-modal-tar';
 var active = 'data-is-active';
 // -----------------------------------------------
+// setup
+// -----------------------------------------------
+if($('[' + tar + ']').length) {
+	$('[data-modal-tar]').append('<div data-modal-close>ｆｄｓｆ</div>');
+}
+// -----------------------------------------------
 // function
 // -----------------------------------------------
+$('[' + trg + ']').each(function() {
+	if($(this).attr(active)) {
+		var id = $(this).attr(trg);
+		$('[' + trg + '~=' + id + ']').attr(active, 'true');
+		$('[' + tar + '~=' + id + ']').attr(active, 'true');
+		overlay(true);
+		return false;
+	}
+});
+/* ------------------- */
 $('[' + trg + ']').click(function() {
 	var id = $(this).attr(trg);
 	if($(this).attr(active) == 'true') {
@@ -27,13 +43,28 @@ $('[' + trg + ']').click(function() {
 		overlay(true);
 	}
 });
+/* ------------------- */
 $(window).resize(function() {
 	$('[' + trg + ']').attr(active, 'false');
 	$('[' + tar + ']').attr(active, 'false');
 	overlay(false);
 });
+/* ------------------- */
+$('[data-modal-close]').click(function() {
+	$('[' + trg + ']').attr(active, 'false');
+	$('[' + tar + ']').attr(active, 'false');
+	overlay(false);
+});
+/* ------------------- */
 $('[data-overlay-layer]').click(function() {
 	$('[' + trg + ']').attr(active, 'false');
 	$('[' + tar + ']').attr(active, 'false');
 	overlay(false);
 });
+/* ------------------- */
+$.fn.modal = function() {
+	var id = $(this).attr(trg);
+	$('[' + trg + '~=' + id + ']').attr(active, 'true');
+	$('[' + tar + '~=' + id + ']').attr(active, 'true');
+	overlay(true);
+};
