a1447ce79cc44b44b068b3dfb4293dbc004c5198
taku <takulin8080@gmail.com>
2018-03-29 12:23:16 +0900

    [update] webpack setting 調整
    app.js → bundle.js　名称変更

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -1,24 +1,15 @@
-var path = require("path");
-var webpack = require("webpack");
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require('path');
 module.exports = {
-	cache: true,
-	entry: {
-		app: './src/common/js/app.js'
-	},
+	// モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+	mode: 'development',
+	// エントリーポイントの設定
+	entry: './src/common/js/app.js',
+	// 出力の設定
 	output: {
-		sourceMapFilename: "./dst/common/js/[name].js.map",
-		filename: '[name].js',
-	},
-	module: {
-		loaders: [{
-			test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
-			loader: 'url-loader'
-		}, {
-			test: /\.html$/,
-			loader: 'html-loader'
-		}, {
-			test: /\.css$/,
-			loaders: ['style-loader', 'css-loader']
-		}, ]
+		// 出力するファイル名
+		filename: 'bundle.js',
+		// 出力先のパス（v2系以降は絶対パスを指定する必要がある）
+		path: path.join(__dirname, 'dst/common/js')
 	}
 };
