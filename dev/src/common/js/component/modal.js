2ce48ced4eebeee7713318255c82acd36cdbf7d5
taku <takulin8080@gmail.com>
2018-07-10 14:09:28 +0900

    [update] 処理内容調整

--- dev/src/common/js/component/modal.js
+++ dev/src/common/js/component/modal.js
@@ -3,94 +3,77 @@
 // ==============================================================================================
 $ = require('jquery');
 overlay = require('../component/overlay');
-// data-modal ========================================
+// variable ========================================
+var trg = 'data-modal-trg';
+var tar = 'data-modal-tar';
+var overlay = $('[data-overlay-layer]');
+var bodyModifier = 'data-modal';
+var toggleData = 'data-is-active';
+$('[' + tar + ']').append('<span data-modal-close></span>');
+var close = $('[data-modal-tar] [data-modal-close]');
+// function ========================================
 $(function() {
-	var trg = 'data-modal-trg';
-	var tar = 'data-modal-tar';
-	var overlay = $('[data-overlay-layer]');
-	var bodyModifier = 'data-modal';
-	var toggleData = 'data-is-active';
-	var id = $('[' + tar + ']').attr(tar);
-	$('[' + tar + '] > div').attr('data-modal-contents', id);
-	$('[' + tar + ']').append('<span data-modal-close></span>');
-	var close = $('[data-modal-tar] [data-modal-close]');
-	if($('[' + trg + ']').length == false) {
+	if($('[' + tar + ']').length == false) {
 		return;
 	} else {
-		var timer = false;
-		var w = $(window).width();
-		var h = $(window).height();
-		var wr;
-		var hr;
-		for(var i = 0; i < $('[' + trg + ']').length; i++) {
-			if($('[' + trg + ']' [i]).attr(toggleData) == 'true') {
-				modalName = $('[' + trg + ']' [i]).attr(trg);
-				toggle(modalName, bodyModifier, 'true');
+		for(var i = 0; i < $('[' + tar + ']').length; i++) {
+			var elmTar = $('[' + tar + ']').eq(i);
+			var id = elmTar.attr(tar);
+			var elmTrg = $('[' + trg + '~=' + id + ']')
+			if(elmTar.attr(toggleData) == 'true') {
+				toggle(id, bodyModifier, 'true');
 			}
 		}
-		$('[' + trg + ']').click(function() {
-			modalName = $(this).attr(trg);
-			if($('body').attr(bodyModifier) == 'true') {
-				disabled();
-			}
-			toggle(modalName, bodyModifier, 'true');
-		});
-		$(window).resize(function() {
-			timer = setTimeout(function() {
-				wr = $(window).width();
-				hr = $(window).height();
-				if(w != wr || h != hr) {
-					toggle(modalName, bodyModifier, 'false');
-				}
-			}, 200);
-		});
-		$('[data-overlay-layer]').click(function() {
-			toggle(modalName, bodyModifier, 'false');
-		});
-		close.click(function() {
-			toggle(modalName, bodyModifier, 'false');
-		});
 	};
-
-	function toggle(name, bodyModifier, boolean) {
-		$('[' + trg + '~=' + name + ']').attr(toggleData, boolean);
-		$('[' + tar + '~=' + name + ']').attr(toggleData, boolean);
-		$('body').attr(bodyModifier, boolean);
-		$('body').attr('data-overlay', boolean);
-	}
-
-	function disabled() {
-		$('[' + tar + ']').attr(toggleData, 'false');
-		$('[' + trg + ']').attr(toggleData, 'false');
-		$('body').attr(bodyModifier, 'false');
-		$('body').attr('data-overlay', 'false');
+});
+/* ---------------------------------------- */
+$('[' + trg + ']').click(function() {
+	if($('body').attr(bodyModifier) == 'true') {
+		disabled();
+	} else {
+		var id = $(this).attr(trg);
+		toggle(id, bodyModifier, 'true');
 	}
 });
-// function ========================================
 $.fn.modal = function() {
-	var id = $(this).attr('data-modal-tar');
-	var trg = $('[data-modal-trg="' + id + '"]');
-	var tar = $('[data-modal-tar="' + id + '"]');
-	var overlay = $('[data-overlay-layer]');
-	var bodyModifier = 'data-modal';
-	var toggleData = 'data-is-active';
-	$(this).attr('data-modal-contents', id);
-	tar.append('<span data-modal-close></span>');
-	var close = $('[data-modal-tar] [data-modal-close]');
-	/* ======================================== */
-	toggle(bodyModifier, 'true');
-	/* ======================================== */
-	$('[data-overlay-layer]').click(function() {
-		toggle(bodyModifier, 'false');
-	});
-	close.click(function() {
-		toggle(bodyModifier, 'false');
-	});
-
-	function toggle(bodyModifier, boolean) {
-		trg.attr(toggleData, boolean);
-		tar.attr(toggleData, boolean);
-		$('body').attr(bodyModifier, boolean);
-		$('body').attr('data-overlay', boolean);
-	}
+	var id = $(this).attr(tar);
+	$('[' + tar + '~=' + id + '] > div').attr('data-modal-contents', id);
+	toggle(id, bodyModifier, 'true');
 };
+/* ---------------------------------------- */
+overlay.click(function() {
+	disabled()
+});
+/* ---------------------------------------- */
+close.click(function() {
+	disabled()
+});
+/* ---------------------------------------- */
+$(window).resize(function() {
+	var timer = false;
+	var w = $(window).width();
+	var h = $(window).height();
+	var wr;
+	var hr;
+	timer = setTimeout(function() {
+		wr = $(window).width();
+		hr = $(window).height();
+		if(w != wr || h != hr) {
+			disabled()
+		}
+	}, 200);
+});
+// common fucntion ========================================
+function toggle(id, bodyModifier, boolean) {
+	$('[' + trg + '~=' + id + ']').attr(toggleData, boolean);
+	$('[' + tar + '~=' + id + ']').attr(toggleData, boolean);
+	$('body').attr(bodyModifier, boolean);
+	$('body').attr('data-overlay', boolean);
+}
+/* ---------------------------------------- */
+function disabled() {
+	$('[' + tar + ']').attr(toggleData, 'false');
+	$('[' + trg + ']').attr(toggleData, 'false');
+	$('body').attr(bodyModifier, 'false');
+	$('body').attr('data-overlay', 'false');
+}
