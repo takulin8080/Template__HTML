c126cc7e14decdbf24eea8eafe49f3e7aa894dee
taku <takulin8080@gmail.com>
2018-07-12 17:58:14 +0900

    [update] 構成, スタイル調整

--- dev/src/common/js/component/loading.js
+++ dev/src/common/js/component/loading.js
@@ -3,7 +3,8 @@
 // ==============================================================================================
 // setup ========================================
 $('body').attr('data-loading', 'true');
-$('body').append('<div data-loading-overlay></div>');
+$('body').append('<div data-loading-layer><div data-loading-animation></div></div>');
+// function ========================================
 $(window).on('load', function() {
 	$('body').attr('data-loading', 'false');
 });
