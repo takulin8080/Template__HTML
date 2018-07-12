ac28be8a25a27bfb4800d39edc7b373f4c42431b
taku <takulin8080@gmail.com>
2018-07-12 14:07:09 +0900

    [update] 変数調整

--- dev/src/common/js/component/pagescroll.js
+++ dev/src/common/js/component/pagescroll.js
@@ -5,7 +5,7 @@
 $ = require('jquery');
 // variable ========================================
 var offsetY = -10;
-var time = 500;
+var time = 400;
 // function ========================================
 $('a[href^="#"]').click(function() {
 	var target = $(this.hash);
@@ -13,7 +13,7 @@ $('a[href^="#"]').click(function() {
 	var targetY = target.offset().top + offsetY;
 	$('html,body').animate({
 		scrollTop: targetY
-	}, time, 'swing');
+	}, durationTime1, 'swing');
 	window.history.pushState(null, null, this.hash);
 	return false;
 });
--- dev/src/common/js/foundation/variable.js
+++ dev/src/common/js/foundation/variable.js
@@ -5,3 +5,7 @@
 breakpointSm = '480';
 breakpointMd = '768px';
 breakpointLg = '1000px';
+// duration time ========================================
+durationTime1 = 400;
+durationTime2 = 800;
+durationTime3 = 1600;
