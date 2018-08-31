f8ed98a0fa922a2fa19e341628a4fa559e1b32f2
taku <takulin8080@gmail.com>
2018-08-31 17:58:02 +0900

    [update] ovaerlay 起動時にページスクロールlock

--- dev/src/common/js/component/overlay.js
+++ dev/src/common/js/component/overlay.js
@@ -7,5 +7,16 @@ $('body').append('<div data-overlay-layer></div>');
 overlayLayer = $('[data-overlay-layer]');
 // function ========================================
 module.exports = function(boolean) {
+	scrollpos = $(window).scrollTop();
+	if(boolean) {
+		$('body').css({
+			'top': -scrollpos
+		});
+		window.scrollTo(0, scrollpos);
+	} else {
+		$('body').css({
+			'top': 0
+		});
+	}
 	$('body').attr('data-overlay', boolean);
 }
