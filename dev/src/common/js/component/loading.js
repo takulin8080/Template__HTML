cd9e95ca0623baf683a45659ef1e2bf5502e9089
taku <takulin8080@gmail.com>
2018-09-03 14:18:52 +0900

    [clean] 命名規則変更

--- dev/src/common/js/component/loading.js
+++ dev/src/common/js/component/loading.js
@@ -2,9 +2,9 @@
 // component/loading
 // ==============================================================================================
 // setup ========================================
-$('body').attr('data-loading', 'true');
+$('body').attr('data-loading', 'false');
 $('body').append('<div data-loading-layer><div data-loading-animation></div></div>');
 // function ========================================
 $(window).on('load', function() {
-	$('body').attr('data-loading', false);
+	$('body').attr('data-loading', true);
 });
