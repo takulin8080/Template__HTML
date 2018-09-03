eb12bdd3a0a101a5350fb151cc1605115e1b12b4
taku <takulin8080@gmail.com>
2018-09-03 12:18:12 +0900

    [clean]  命名規則調整

--- dev/src/common/js/component/active.js
+++ dev/src/common/js/component/active.js
@@ -9,27 +9,36 @@ var booleanData = 'data-is-active';
 // function ========================================
 $('[' + trgData + ']').click(function() {
 	var id = $(this).attr(trgData).replace(/toggle/g, '').replace(/add/g, '').replace(/remove/g, '').replace(/\s+/g, '');
-	var falseGroup = function(i) {
-		if(i.closest('[' + parentData + ']').length) {
-			var parent = i.closest('[' + parentData + ']');
-			parent.find('[' + tarData + ']').attr(booleanData, 'false');
-			parent.find('[' + trgData + ']').attr(booleanData, 'false');
-		}
-	}
 	if(~$(this).attr(trgData).indexOf('add')) {
 		falseGroup($(this));
-		$('[' + tarData + '~=' + id + ']').attr(booleanData, 'true');
+		$('[' + tarData + '~=' + id + ']').attr(booleanData, true);
 	} else if(~$(this).attr(trgData).indexOf('remove')) {
 		falseGroup($(this));
-		$('[' + tarData + '~=' + id + ']').attr(booleanData, 'false');
+		$('[' + tarData + '~=' + id + ']').attr(booleanData, false);
 	} else {
 		if($(this).attr(booleanData) == 'true') {
-			$(this).attr(booleanData, 'false');
-			$('[' + tarData + '~=' + id + ']').attr(booleanData, 'false');
+			$(this).attr(booleanData, false);
+			$('[' + tarData + '~=' + id + ']').attr(booleanData, false);
 		} else {
 			falseGroup($(this));
-			$(this).attr(booleanData, 'true');
-			$('[' + tarData + '~=' + id + ']').attr(booleanData, 'true');
+			$(this).attr(booleanData, true);
+			$('[' + tarData + '~=' + id + ']').attr(booleanData, true);
 		}
 	}
 });
+/* common ---------------------------------------- */
+function toggle(e) {
+	if(e.closest('[' + parentData + ']').length) {
+		var parent = e.closest('[' + parentData + ']');
+		parent.find('[' + tarData + ']').attr(booleanData, false);
+		parent.find('[' + trgData + ']').attr(booleanData, false);
+	}
+}
+
+function falseGroup(e) {
+	if(e.closest('[' + parentData + ']').length) {
+		var parent = e.closest('[' + parentData + ']');
+		parent.find('[' + tarData + ']').attr(booleanData, false);
+		parent.find('[' + trgData + ']').attr(booleanData, false);
+	}
+}
